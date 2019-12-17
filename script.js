8c55f0ec4d402fa8fe90a15acbbfa9ef28ce4bc9
Alexander Talberg <alexandermtalberg@gmail.com>
2019-12-17 14:41:37 -0700

    Made the operators print correctly

--- script.js
+++ script.js
@@ -3,15 +3,47 @@ var result = $("#results")
 var topNum = $("#first-number")
 var operator = $("#operator")
 var bottomNum = $("#second-number")
+// var currentBox = $('.printHere')
 
 var numBtn = $(".number")
 var opBtn = $(".operator")
 var equalBtn = $(".equal")
 var clearBtn = $(".clear")
 
-numBtn.on("click", function(e) {
+
+
+
+
+
+
+
+numBtn.on("click", function (e) {
     e.preventDefault();
     var firstNumber = ($(this).attr("value"))
     topNum.append(firstNumber)
 
 })
+
+opBtn.on('click', function (e) {
+    e.preventDefault();
+    var opNum = ($(this).attr("value"))
+    operator.append(opNum)
+
+    if (($(this).attr('value')) === 'plus') {
+        opNum = '+';
+    }
+    else if (($(this).attr('value')) === 'minus')
+     { opNum = '-'; }
+    else if (($(this).attr('value')) === 'times')
+     { opNum = 'x'; }
+    else if (($(this).attr('value')) === 'divide')
+     { opNum = '/';
+    }
+
+    else { opNum = '^'; } console.log(opNum);
+});
+
+
+
+
+
