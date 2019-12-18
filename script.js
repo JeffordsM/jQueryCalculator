ddcaba5ec33b0a2e3f27227c388187af95072b91
Choop <matthewjeffords97@yahoo.com>
2019-12-18 10:04:33 -0700

    matt's commit

--- script.js
+++ script.js
@@ -3,15 +3,54 @@ var result = $("#results")
 var topNum = $("#first-number")
 var operator = $("#operator")
 var bottomNum = $("#second-number")
+var pushNum = $("#first-number")
 
 var numBtn = $(".number")
 var opBtn = $(".operator")
 var equalBtn = $(".equal")
 var clearBtn = $(".clear")
 
+var opHit = 0;
+var equation = "";
+
+
 numBtn.on("click", function(e) {
     e.preventDefault();
     var firstNumber = ($(this).attr("value"))
-    topNum.append(firstNumber)
-
+    pushNum.append(firstNumber)
 })
+
+opBtn.on("click", function(e) {
+    e.preventDefault();
+    if (opHit === 1){
+        return;
+    }
+    pushNum = $("#second-number")
+    var opNum = "";
+    if (($(this).attr("value")) === "plus") {
+        opNum = "+";
+        equation = "+";
+    } 
+    else if (($(this).attr("value")) === "minus") {
+        opNum = "-";
+        equation = "-";
+    }
+    else if (($(this).attr("value")) === "times") {
+        opNum = "x";
+        equation = "*";
+    }
+    else if (($(this).attr("value")) === "divide") {
+        opNum = "/";
+        equation = "/";
+    }
+    operator.text(opNum);
+    opHit++;
+});
+
+equalBtn.on("click", function(e) {
+    e.preventDefault;
+    var x = topNum.text();
+    var y = bottomNum.text();
+    console.log(x + equation + y);
+
+});
